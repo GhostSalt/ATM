f4c0b89cc74e0aedac28996608f60b783aee4fab
GhostSalt <mhanna756@c2ken.net>
2024-12-17 14:52:59 +0000

    -More card stuff!!

--- Assets/Module Scripts/ATMScript.cs
+++ Assets/Module Scripts/ATMScript.cs
@@ -16,16 +16,24 @@ public class ATMScript : MonoBehaviour
     public KMAudio Audio;
     public KMSelectable[] DisplayButtons;
     public KMSelectable[] KeypadKeys;
+    public KMSelectable CardReader;
     public Transform DisplayAudioTransform;
+    public Transform CardTransform;
+    public MeshRenderer[] CardFaceRends;
 
     private Coroutine[] DisplayButtonAnimCoroutines;
     private Coroutine[] KeypadKeyAnimCoroutines;
+    private Vector3 CardInitScale;
     private float DisplayButtonInitPos;
     private float KeypadKeyInitPos;
 
     void Awake()
     {
         _moduleID = _moduleIdCounter++;
+
+        var hue = Rnd.Range(0, 1f);
+        foreach (var rend in CardFaceRends)
+            rend.material.color = Color.HSVToRGB(hue, 1, 0.5f);
 
         DisplayButtonAnimCoroutines = new Coroutine[DisplayButtons.Length];
         DisplayButtonInitPos = DisplayButtons[0].transform.localPosition.y;
@@ -42,17 +50,23 @@ public class ATMScript : MonoBehaviour
             int x = i;
             KeypadKeys[x].OnInteract += delegate { KeypadKeysPress(x); return false; };
         }
+
+        CardReader.OnInteract += delegate { InsertCard(); return false; };
+        CardInitScale = CardTransform.localScale;
+        CardTransform.localScale = Vector3.zero;
     }
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void DisplayButtonPress(int pos)
     {
@@ -72,6 +86,13 @@ public class ATMScript : MonoBehaviour
         if (KeypadKeyAnimCoroutines[pos] != null)
             StopCoroutine(KeypadKeyAnimCoroutines[pos]);
         KeypadKeyAnimCoroutines[pos] = StartCoroutine(ButtonPressAnim(KeypadKeys[pos].transform, KeypadKeyInitPos, 0.002f));
+    }
+
+    private void InsertCard()
+    {
+        CardTransform.localScale = CardInitScale;
+
+        StartCoroutine(InsertCardAnim());
     }
 
     private IEnumerator ButtonPressAnim(Transform target, float start, float depression, float duration = 0.075f)
@@ -95,5 +116,46 @@ public class ATMScript : MonoBehaviour
             timer += Time.deltaTime;
         }
         target.localPosition = new Vector3(target.localPosition.x, start, target.localPosition.z);
+    }
+
+    private IEnumerator InsertCardAnim(float introDuration = 0.5f, float insertDuration = 0.35f, float takeDuration = 0.5f)
+    {
+        Vector3 introEnd = new Vector3(0.0425f, 0.06f, -0.0095f);
+        Vector3 introStart = introEnd + new Vector3(0, 2f, -0.2f);
+
+        Vector3 insertEnd = introEnd - Vector3.up * 0.03f;
+        Vector3 animEnd = insertEnd - Vector3.up * 0.05f;
+
+        CardTransform.localPosition = introStart;
+
+        float timer = 0;
+        while (timer < introDuration)
+        {
+            CardTransform.localPosition = new Vector3(introStart.x, Easing.OutCubic(timer, introStart.y, introEnd.y, introDuration), Easing.OutExpo(timer, introStart.z, introEnd.z, introDuration));
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        CardTransform.localPosition = introEnd;
+
+        timer = 0;
+        while (timer < insertDuration)
+        {
+            CardTransform.localPosition = new Vector3(introEnd.x, Easing.InSine(timer, introEnd.y, insertEnd.y, insertDuration), introEnd.z);
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        CardTransform.localPosition = insertEnd;
+
+        Audio.PlaySoundAtTransform("insert card", CardReader.transform);
+        yield return new WaitForSeconds(0.2f);
+
+        timer = 0;
+        while (timer < takeDuration)
+        {
+            CardTransform.localPosition = new Vector3(insertEnd.x, Easing.InSine(timer, insertEnd.y, animEnd.y, takeDuration), insertEnd.z);
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        CardTransform.localPosition = animEnd;
     }
 }
